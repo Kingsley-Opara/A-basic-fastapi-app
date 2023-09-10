a9de34f079309d1a7e226df412c07f72fe00e859
kingsley <oparaudochukwu277@gmail.com>
2023-09-10 17:14:43 +0100

    make changes in the utctime and current day

--- app/main.py
+++ app/main.py
@@ -1,14 +1,34 @@
 from fastapi import FastAPI
 from datetime import datetime
+from datetime import date
+
+today = date.today()
 
 app = FastAPI()
+
+def get_the_current_weekday():
+    weekday = today.weekday()
+    if weekday == 0:
+        return 'Monday'
+    elif weekday == 1:
+        return 'Tuesday'
+    elif weekday == 2:
+        return 'Wednesday'
+    elif weekday == 3:
+        return 'Thursday'
+    elif weekday == 4:
+        return 'Friday'
+    elif weekday == 5:
+        return 'Saturday'
+    return 'Sunday'
+
 
 @app.get('/api', status_code=200)
 async def home(slack_name: str = 'Kingsley Udochukwu', track: str = 'backend'):
     hng_profile = {
         "slack_name": slack_name,
-        "current_day": "Saturday",
-        "utc_time": datetime.utcnow(),
+        "current_day": get_the_current_weekday(),
+        "utc_time": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
         "track": track,
         "github_file_url": 'https://github.com/Kingsley-Opara/A-basic-fastapi-app/blob/main/app/main.py',
         "github_repo_url": "https://github.com/Kingsley-Opara/A-basic-fastapi-app/",
